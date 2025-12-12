b5957ef789c898c244500c4f8943ae7801390622
Dipan <dipanmaulick25@gmail.com>
2025-12-12 15:27:56 +0000

    added another command to read the no of words

--- 02-cli-1/index.js
+++ 02-cli-1/index.js
@@ -1,26 +1,43 @@
-//CLI to count the no of lines... CLI HAS commands
+//CLI to count the no of lines and words... CLI HAS commands
 const fs = require("fs");
-const {Command} = require("commander")
+const {Command} = require("commander") //package to create cli- based on commands
 const program = new Command();
 
-program
+program      //description of the entire CLI
     .name('File related CLI')
-    .description("CLI to count the no of lines in a file")
+    .description("CLI to count the no of lines and words in a file")
     .version('0.7.0')
 
-program
-    .command('count')
+program //1ST COMMAND
+    .command('count-lines')
     .description("Count the no of lines")
-    .argument("<file" , "file from which to count")
+    .argument("<file>" , "file from which to count")
     .action((file)=>{
         fs.readFile(file, 'utf-8' , (err,data)=>{
             if (err){
                 console.log(err)
             }else{
-                const lines = data.split('\n').length;
+                const lines = data.split('\n').length
                 console.log(`There are ${lines} lines in ${file}`)
             }
         })
     })
 
-program.parse();
+program //2ND COMMAND
+    .command('count-words')
+    .description("Count the no of words")
+    .argument("<file>" , "file from which to count")
+    .action((file)=>{
+        fs.readFile(file, 'utf-8' , (err,data)=>{
+            if (err){
+                console.log(err)
+            }else{
+                const words = data.split(/\s+/).length;
+                console.log(`There are ${words} words in ${file}`)
+            }
+        })
+    })
+
+
+
+program.parse(); //look at what we provided, matching with the command, and execute the correct action
