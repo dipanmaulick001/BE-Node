55902481d2d8dcb5412f709ad2e5595a9e5238f4
Dipan <dipanmaulick25@gmail.com>
2025-12-16 21:07:44 +0000

    a basic server with query inputs

---
+++ 10-server-w-query/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+app.get("/sum" , ( req , res)=>{
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans : a + b
+    })
+
+})
+
+app.get("/multiply", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a * b
+    })
+});
+
+
+
+app.get("/divide" , ( req , res)=>{
+    const a = parseInt(req.query.a) ; 
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans : a/b
+    })
+
+})
+
+app.get("/subtract" , ( req , res)=>{
+
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans : a - b
+    })
+
+})
+
+
+
+app.listen(5500);
