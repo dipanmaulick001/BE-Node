a8ff07ed4333de3ddf4f3abf92495657120f8a78
Dipan <dipanmaulick25@gmail.com>
2025-12-20 06:49:04 +0000

    created express apps(one slightly diff syntax),used tokens

---
+++ 18-auth/index2.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+
+const users = []; //use it as a database ,store here after user signs up
+
+app.use(express.json());
+
+//random token generated after sign in 
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+app.post("/signup" , function(req , res){
+    //later we add input validation with zod
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username : username,
+        password : password
+    })
+
+    //later on, add logic like if account exists, cant make another acc
+
+    res.json({
+        msg : "You have created an account ! Congrats"
+    })
+
+})
+
+app.post("/signin" , (req , res)=>{
+    //when signin hit, token given and stored in users
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+
+    for( let i =0; i<users.length ; i++){
+            if(users[i].username == username && users[i].password == password){
+                foundUser = users[i]
+            }
+    }
+
+    if (foundUser){
+        const token = generateToken();
+        res.json({
+            token : token
+        })
+    }else{
+        res.json({msg : "credentials dont match"})
+    }
+
+})
+
+app.listen(3003);
